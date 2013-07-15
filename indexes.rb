5b8eb9ef7125559bb54ce6e4fdb3b29dba857b22
Kavya Joshi <kavya@Kavyas-MacBook-Air.local>
2013-07-15 16:56:34 -0400

    Indexes cleaned up

--- indexes.rb
+++ indexes.rb
@@ -13,57 +13,60 @@ ZIPCODE = '94114'
 NAV_FRAME = 1
 MAIN_FRAME = 2
 
-def generate_indexes
+class SafewayIndexGenerator
+    def initialize    
+        @client = Selenium::WebDriver::Remote::Http::Default.new
+        @client.timeout = 180 # seconds (default is 60)
 
-    # puts "GO TO AISLE URL"
-    browser = Watir::Browser.new :chrome
-
-    browser.goto SAFEWAY_URL # Watir waits
-
-    browser.as(:href, GROCERY_URL).first.click
-    browser.window(:title, GROCERY_TITLE).wait_until_present
-    url = browser.window(:title, GROCERY_TITLE).url
-    browser.window(:title, GROCERY_TITLE).close
-    browser.goto url
-
-    browser.as(:href, AISLE_URL).first.click # Need to register and re-try
-
-    browser.text_field(:name, "Register.ZipCode").set ZIPCODE
-    browser.divs(:class, "btn btn-browseasguest").first.click
-    browser.as(:href, AISLE_URL).first.click
-
-    puts "NOW, GENERATE_INDEXES"
-
-    num_aisles = browser.frames[NAV_FRAME].as.size
-
-    puts "NUM_AISLES = ", num_aisles
+        @browser = Watir::Browser.new :chrome, :http_client=>client
+    end
     
-    aisle_dict = Array.new(num_aisles)
-    (0...num_aisles).to_a.each do |i|
-        browser.goto AISLE_URL
-	browser.frames[NAV_FRAME].as[i].click
-	num_subaisles = browser.frames[NAV_FRAME].as.size
-     	aisle_dict[i] = num_subaisles
-     end
+    def goto_aisles
+        @browser.goto SAFEWAY_URL # Watir waits
 
-     puts "AISLE_DICT", aisle_dict
+        @browser.as(:href, GROCERY_URL).first.click
+        @browser.window(:title, GROCERY_TITLE).wait_until_present
+        @url = browser.window(:title, GROCERY_TITLE).url
+        @browser.window(:title, GROCERY_TITLE).close
+        @browser.goto url
 
-     subaisle_dict = Hash.new
-     (0...num_aisles).to_a.each do |i|
-     	 (2...aisle_dict[i]).to_a.each do |j|
-             browser.goto AISLE_URL
-	     browser.frames[NAV_FRAME].as[i].click
-	     browser.frames[NAV_FRAME].as[j].click
-	     num_shelfs = browser.frames[NAV_FRAME].as.size
-	     subaisle_dict[[i, j]] = num_shelfs
-	     puts "i,j = ", [i, j]
-	     puts "NUM_SHELFS = ", num_shelfs
-	 end
-     end
+        @browser.as(:href, AISLE_URL).first.click # Need to register and re-try
 
-     return num_aisles, subaisle_dict
+        @browser.text_field(:name, "Register.ZipCode").set ZIPCODE
+        @browser.divs(:class, "btn btn-browseasguest").first.click
+        @browser.as(:href, AISLE_URL).first.click
+    end
+
+    def generate_indexes
+        @num_aisles = @browser.frames[NAV_FRAME].as.size
+        
+        @aisle_dict = Array.new(num_aisles)
+        (0...num_aisles).to_a.each do |i|
+            @browser.goto AISLE_URL
+	        @browser.frames[NAV_FRAME].as[i].click
+	        num_subaisles = @browser.frames[NAV_FRAME].as.size
+     	    @aisle_dict[i] = num_subaisles
+        end
+
+        @subaisle_dict = Hash.new
+        (0...num_aisles).to_a.each do |i|
+     	     (2...aisle_dict[i]).to_a.each do |j|
+                @browser.goto AISLE_URL
+	            @browser.frames[NAV_FRAME].as[i].click
+	            @browser.frames[NAV_FRAME].as[j].click
+	            num_shelfs = @browser.frames[NAV_FRAME].as.size
+	            @subaisle_dict[[i, j]] = num_shelfs
+	        end
+        end
+
+        return @num_aisles, @aisle_dict, @subaisle_dict
+    end
 end
 
-a, b = generate_indexes
-puts a
-puts b
+generator = SafewayIndexGenerator.new
+generator.goto_aisles
+numaisles, aisledict, subaisledict = generator.generate_indexes
+
+puts numaisles
+puts aisledict
+puts subaisledict
